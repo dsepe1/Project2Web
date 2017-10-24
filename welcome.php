7cb8f3e4a8814fe6c70ce251eaf75a43d93ffdb4
GSM1900\NGarcia143 <nestor.garcia143@T-Mobile.com>
2017-10-24 17:45:57 -0400

    Initial commit

---
+++ welcome.php
@@ -0,0 +1,19 @@
+<?php
+include("header.inc");
+?>
+
+    <meta name="Keywords" content="KEYWORD1, KEYWORD2, KEYWORD3">
+    <meta name="Project 2" content="Welcome Page">
+    <title>Welcome!</title>
+
+<?php
+include("body.inc");
+?>
+
+    <div id="content">
+        Welcome to our webpage!
+    </div>
+
+<?php
+include("footer.inc");
+?>
